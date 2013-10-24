d6dd3907a2f43454b939adf8ec6b10fbb60eeb08
seanstory <sean.j.story@vanderbilt.edu>
2013-10-24 15:14:37 -0500

    Initial Commit Added the default/basic mvn project

---
+++ impl/Asgn5/src/main/java/edu/vanderbilt/cs278/Asgn5/App.java
@@ -0,0 +1,13 @@
+package edu.vanderbilt.cs278.Asgn5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
